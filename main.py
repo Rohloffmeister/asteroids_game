d05e3e413200606a309e39d46243a7b3f6f24e48
Rohloffmeister <sparkassenraeuber@gmail.com>
2024-12-31 03:08:06 +0100

    Added Asteroid and Asteroidfield Classes and implementation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print("Starting asteroids!")
@@ -12,28 +14,33 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Player.containers = (updatable,drawable)
+    
+    asteroidfield = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     
     while True:
-        screen.fill(pygame.Color(0,0,0))
-        player.draw(screen)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
-        player.update(dt)
-        
-        
-        
-        
+            
+            
+        for object in updatable:
+            object.update(dt)
         
         
-        
-        
-        
-        
-        
+        screen.fill(pygame.Color(0,0,0))
+ 
+        for object in drawable:
+            object.draw(screen)
+
         
         
         
